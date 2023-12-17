26032f22dd2c4765f402faefccd0b7a8d15c4ceb
Jessica Daiana <daianalogy@gmail.com>
2023-12-17 12:03:23 -0400

    cmus add

---
+++ cmus/lib.pl
@@ -0,0 +1,27 @@
+/home/reithe/Music/Apollo 3 - Überflieger.mp3
+/home/reithe/Music/CL - ONE AND ONLY.mp3
+/home/reithe/Music/02 PING PONG.m4a
+/home/reithe/Downloads/Telegram Desktop/9MUSES - News.flac
+/home/reithe/Music/AFI - Love Like Winter.flac
+/home/reithe/Downloads/Telegram Desktop/After School - AH.flac
+/home/reithe/Music/After School - DIVA.flac
+/home/reithe/Music/BIGBANG - Lies.flac
+/home/reithe/Music/BIGBANG - Wonderful.flac
+/home/reithe/Music/10 Mirror.m4a
+/home/reithe/Music/Dreamcatcher - BOCA.flac
+/home/reithe/Music/02 Given-Taken.m4a
+/home/reithe/Music/11 Snapshot.m4a
+/home/reithe/Music/In2it - SnapShot.flac
+/home/reithe/Music/02 Jumping (점핑 (Jumping)).m4a
+/home/reithe/Music/Los Mesoneros - Miedo Y Ficción.flac
+/home/reithe/Music/Moon Ray - Comanchero (Vocal Radio).flac
+/home/reithe/Music/SEVENTEEN - Super.flac
+/home/reithe/Music/SF9 - Trauma.flac
+/home/reithe/Downloads/Telegram Desktop/t.A.T.u. - Досчитай до ста.flac
+/home/reithe/Downloads/Telegram Desktop/Tokio Hotel - Für immer jetzt.flac
+/home/reithe/Downloads/Telegram Desktop/Tokio Hotel - Kampf der Liebe.flac
+/home/reithe/Downloads/Telegram Desktop/Tokio Hotel - Hunde.flac
+/home/reithe/Music/03 Time To Go.m4a
+/home/reithe/Music/U-KISS - NEVERLAND.flac
+/home/reithe/Music/U-KISS - Obsession.flac
+/home/reithe/Music/U-KISS - 숨도 못 쉬어.flac
